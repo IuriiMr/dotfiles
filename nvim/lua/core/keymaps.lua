676972d17d7d1b8abdf1e943a6a7e51cfcbd3fa4
IuriiMr <iurii.marushevskyi@implex.dev>
2023-09-23 18:01:52 +0200

    Add files via upload

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,18 @@
+vim.g.maplider = ' '
+vim.g.maplocaleader = ' '
+
+vim.opt.number = true
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+-- vim.keymap.set('n', '<c-l>', ':nohlsearch<CR>')
